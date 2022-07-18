5410412abd7937b99809e8ae101aac9784dd1815
xyjjyyy <904588748@qq.com>
2022-07-19 02:00:01 +0800

    update

--- app.py
+++ app.py
@@ -9,29 +9,29 @@ import numpy as np
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route("/", methods=["GET", "POST"])
 def upload_file():
-    if request.method == 'POST':
-        file = request.files['file']
+    if request.method == "POST":
+        file = request.files["file"]
         print("File Received")
         filename = secure_filename(file.filename)
 
         print(filename)
         file.save("static/" + filename)
-        # Heroku no need static
-        # file = open("static/“+ filename,”r")
         model = load_model("Chest")
-        img = Image.open("static/"+filename)  
-        img = img.resize((100, 100))
-        # need to transfer to np to reshape
+        image = cv2.imread("static/" + filename)
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        img = cv2.merge([gray, gray, gray])
+        img.resize((100, 100, 3))
+
         img = np.asarray(img, dtype="float32")
         # rgb to reshape to 1,100,100,3
         img = img.reshape(1, img.shape[0], img.shape[1], img.shape[2])
         img.shape
         pred = model.predict(img)
-        return(render_template("index.html", result=str(pred)))
+        return render_template("index.html", result=str(pred))
     else:
-        return(render_template("index.html", result="2"))
+        return render_template("index.html", result="none")
 
 
 if __name__ == "__main__":
