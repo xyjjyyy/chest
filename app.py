30d4ee50d115edfe35470df471323066315fb24a
xyjjyyy <904588748@qq.com>
2022-07-22 00:33:25 +0800

    app.py updatedd

--- app.py
+++ app.py
@@ -1,35 +1,29 @@
 from flask import Flask, render_template, request
 from werkzeug.utils import secure_filename
-from skimage import io
 from keras.models import load_model
-import cv2
-from PIL import Image  # use PIL
+#import cv2
+from PIL import Image #use PIL
 import numpy as np
 
 app = Flask(__name__)
 
-
-@app.route("/", methods=["GET", "POST"])
-def upload_file():
-    if request.method == "POST":
-        file = request.files["file"]
+@app.route('/', methods=['GET', 'POST'])
+def init():
+    if request.method == 'POST':
+        file = request.files['file']
         print("File Received")
         filename = secure_filename(file.filename)
-
         print(filename)
-        file.save("static/" + filename)
-        image = Image.open("static/"+filename)
+        # Open the image form working directory
+        image = Image.open(file)
         model = load_model("Chest")
         img = np.asarray(image)
         img.resize((100,100,3))
         img = np.asarray(img, dtype="float32") #need to transfer to np to reshape
         img = img.reshape(1, img.shape[0], img.shape[1], img.shape[2]) #rgb to reshape to 1,100,100,3
         pred=model.predict(img)
-        print(str(pred))
-        return render_template("index.html", result=str(pred))
+        return(render_template("index.html", result=str(pred)))
     else:
-        return render_template("index.html", result="none")
-
-
+        return(render_template("index.html", result="WAITING"))
 if __name__ == "__main__":
     app.run()
